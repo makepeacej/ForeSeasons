ea5252c7d27f49cc91ece13cc652a4adc9fdcc89
ParticalRay <47603577+ParticalRay@users.noreply.github.com>
2023-01-01 21:14:41 -0800

    Overrided the default login window views to make adjustments. May erase unneccessary views later to save space.

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -22,7 +22,7 @@ namespace Appointment_Scheduler.Controllers
 
 
         // GET: Appointments
-        [Authorize]
+        
         public async Task<IActionResult> Index()
         {
 
---
+++ Data/Migrations/20221223155852_login.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appointment_Scheduler.Data.Migrations
+{
+    public partial class login : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
